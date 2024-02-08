28024d51d6c6d91234d26ce6c75bcc2895c043ca
AabharanGupta <aabharangupta1001@gmail.com>
2024-02-08 14:04:33 +0530

    arcane

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,6 @@ import {Router} from 'express';
 import ReckonRegistration from '../controllers/reckon.controller.js';
 import CircuitrixRegistration from '../controllers/circuitrix.controller.js';
 import BuzzingaRegistration from '../controllers/buzzinga.controller.js';
-import ArcaneRegistration from '../controllers/arcane.controller.js';
 import FallacyRegistration from '../controllers/fallacy.controller.js';
 
 const router = Router();
@@ -11,7 +10,6 @@ const router = Router();
 router.route('/reckon').post(ReckonRegistration);
 router.route('/circuitrix').post(CircuitrixRegistration);
 router.route('/buzzinga').post(BuzzingaRegistration);
-router.route('/arcane').post(ArcaneRegistration);
 router.route('/fallacy').post(FallacyRegistration);
 
 export default router;
