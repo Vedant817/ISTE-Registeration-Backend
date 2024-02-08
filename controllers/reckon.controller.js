b13852a8296c517e173234d7e6879854b2175620
AabharanGupta <aabharangupta1001@gmail.com>
2024-02-08 22:00:32 +0530

    ctf

--- controllers/reckon.controller.js
+++ controllers/reckon.controller.js
@@ -3,7 +3,7 @@ import {Reckon} from '../models/reckon.model.js';
 const reckonRegistration = async (req, res) => {
     const {leaderName, leaderEmail, leaderPhone, teamName, member2Name, member2Email, member2Phone, member3Name, member3Email, member3Phone} = req.body;
     try {
-        const team = new Reckon({leaderName, leaderEmail, leaderPhone, teamName, member2Name, member2Email, member2Phone, member3Name, member3Email, member3Phone, member4Name, member4Email, member4Phone});
+        const team = new Reckon({leaderName, leaderEmail, leaderPhone, teamName, member2Name, member2Email, member2Phone, member3Name, member3Email, member3Phone});
         const savedTeam = await team.save();
         res.status(201).json({status: true,message: 'Registered Successfully!!'});
     } catch (error) {
